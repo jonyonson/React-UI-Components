b07304242694ad114b4191c29161967879ed4a0e
Jonathan Taylor <jonyonson@icloud.com>
2019-04-22 17:56:29 -0500

    added momemt.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 function HeaderTitle() {
   return (
     <div className="HeaderTitle">
       <span className="HeaderTitle__name">Lambda School </span>
       <span className="HeaderTitle__handle">@LambdaSchool</span> ·
-      <span className="HeaderTitle__date"> 22 apr</span>
+      <span className="HeaderTitle__date">
+        {' '}
+        {moment()
+          .startOf('hour')
+          .fromNow()}
+      </span>
     </div>
   );
 }
